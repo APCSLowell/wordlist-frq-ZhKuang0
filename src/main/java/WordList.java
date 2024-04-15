27219130c4722b116ad3a2a92f333fbc96688270
ZhKuang0 <143749890+ZhKuang0@users.noreply.github.com>
2024-04-15 11:34:55 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -8,22 +8,33 @@ public class WordList
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-    int num = 0;
-    for (String blank: this.myList)
-      if (blank.length() == len)
-    num++;
-    return num;
-  }
+  public int numWordsOfLength(int length)
+{
+    int numWords = 0;
 
-  public void removeWordsOfLength(int len)
-  {
-    for (int i = 0; i < this.myList.size()-1; i++) {
-      if (this.myList.get(i).length() == len) {
-      this.myList.remove(i);
-        i--;
-      }
+    for(int i = 0; i < myList.size(); i++)
+    {
+        String word = (String) myList.get(i);
+        
+        if(word.length() == length)
+            numWords++;
     }
-  }
+
+    return numWords;
+}
+
+  public void removeWordsOfLength(int length)
+{
+    int i = 0;
+
+    while(i < myList.size())
+    {
+        String word = (String) myList.get(i);
+        
+        if(word.length() == length)
+            myList.remove(i);
+        else
+            i++;
+    }
+}
 }
